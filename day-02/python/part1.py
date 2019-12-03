704c213616dfc8a0e3eedb6d2cd02386ea5dcd24
Aaron Barnett <aaron.barnett@outlook.com>
2019-12-02 22:30:25 -0800

    Part 1 solution!
    * Actual tests!
    * The solution!
    * A readme explaining how to run it

--- day-02/python/part1.py
+++ day-02/python/part1.py
@@ -1,5 +1,8 @@
-from lib import read_from_input
+from intcode_lib import process_1202_program, process_intcode, read_from_input
 
-intcode = read_from_input()
+intcode_from_file = read_from_input()
+intcode_input = process_1202_program(intcode_from_file)
+processed_intcode = process_intcode(intcode_input)
 
-print(intcode)
+print(processed_intcode[0])
+
