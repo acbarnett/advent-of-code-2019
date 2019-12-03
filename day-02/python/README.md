59331781f826062597572aab4342790dcc908807
Aaron Barnett <aaron.barnett@outlook.com>
2019-12-02 22:30:25 -0800

    Part 1 solution!
    * Actual tests!
    * The solution!
    * A readme explaining how to run it

---
+++ day-02/python/README.md
@@ -0,0 +1,17 @@
+# Day 02 Python Solution #
+
+## Running the tests ##
+
+For day 2, let's add some unit tests to this business!
+Using python's `unittest` module, this is very easy.
+
+To run the tests, just call `python3 intcode_lib_test.py`
+
+These tests run through the test cases supplied in the README for part 1 of today's challenge.
+
+## Solving part 1 ##
+
+We just need to get the value in the first position.
+The file `part1.py` has the solution for this.
+
+Invoke it with `python3 part1.py` and you should be good to go!
--- day-02/python/intcode_lib.py
+++ day-02/python/intcode_lib.py
@@ -8,8 +8,34 @@ def read_from_input():
         intcode_list = [int(i) for i in intcode_string.split(",")]
         return intcode_list
 
+def process_1202_program(intcode_input):
+    intcode = intcode_input[:]
+    intcode[1] = 12
+    intcode[2] = 2
+    return intcode
+
+END_OP = 99
+ADD_OP = 1
+MULT_OP = 2
+
 # Given a list of intcode, resolve it into its final form
 def process_intcode(intcode_input):
     # Clone the list, to prevent any spooky mutability shenanigans
     intcode = intcode_input[:]
+
+    pos = 0
+    op = intcode[pos]
+
+    while op is not END_OP:
+        input_pos_1 = intcode[pos + 1]
+        input_pos_2 = intcode[pos + 2]
+        dest = intcode[pos + 3]
+        if op is ADD_OP:
+            intcode[dest] = intcode[input_pos_1] + intcode[input_pos_2]
+        if op is MULT_OP:
+            intcode[dest] = intcode[input_pos_1] * intcode[input_pos_2]
+        pos = pos + 4
+        op = intcode[pos]
+
     return intcode
+
