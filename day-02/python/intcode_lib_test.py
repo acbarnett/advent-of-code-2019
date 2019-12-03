31a56e3724716fb093f0aab793fea496d2db1c49
Aaron Barnett <aaron.barnett@outlook.com>
2019-12-02 22:06:40 -0800

    Use actual functions in test file

--- day-02/python/intcode_lib_test.py
+++ day-02/python/intcode_lib_test.py
@@ -1,14 +1,32 @@
 import unittest 
-from intcode_lib import return_four
+from intcode_lib import process_intcode
 
 class IntcodeLibTest(unittest.TestCase): 
-  
-    # Returns True or False.  
-    def test(self):         
-        self.assertTrue(True)
+    def testProcessIntcode1(self):
+        intcode_input = [1,9,10,3,2,3,11,0,99,30,40,50]
+        intcode_output = [3500,9,10,70,2,3,11,0,99,30,40,50]
+        self.assertTrue(process_intcode(intcode_input) == intcode_output)
 
-    def test2(self):
-        self.assertTrue(return_four() == 4)
-  
+    def testProcessIntcode2(self):
+        intcode_input = [1,0,0,0,99]
+        intcode_output = [2,0,0,0,99]
+        self.assertTrue(process_intcode(intcode_input) == intcode_output)
+
+    def testProcessIntcode3(self):
+        intcode_input = [2,3,0,3,99]
+        intcode_output = [2,3,0,6,99]
+        self.assertTrue(process_intcode(intcode_input) == intcode_output)
+
+    def testProcessIntcode4(self):
+        intcode_input = [2,4,4,5,99,0]
+        intcode_output = [2,4,4,5,99,9801]
+        self.assertTrue(process_intcode(intcode_input) == intcode_output)
+
+
+    def testProcessIntcode5(self):
+        intcode_input = [1,1,1,4,99,5,6,0,99]
+        intcode_output = [30,1,1,4,2,5,6,0,99]
+        self.assertTrue(process_intcode(intcode_input) == intcode_output)
+
 if __name__ == '__main__': 
     unittest.main() 
